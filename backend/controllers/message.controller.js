209896b287a3c804635a02fa66dcf9eb1fe943d0
LukeZHar <luke.z.harris@hotmail.com>
2025-11-14 17:44:23 +1000

    Feat: Add getMessage functionality to retrieve messages from a conversation and update sendMessage to save in parallel

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -27,11 +27,34 @@ export const sendMessage = async (req, res) => {
       conversation.message.push(newMessage._id);
     }
 
-    await conversation.save();
-    await newMessage.save();
+    // SOCKET.IO LOGIC TO EMIT MESSAGE IN REAL-TIME ADDED HERE
+
+    // Save both conversation and message in parallel
+    await Promise.all([conversation.save(), newMessage.save()]);
+
     res.status(201).json({ message: "Message sent successfully", newMessage });
   } catch (error) {
     console.error("Error sending message:", error.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("message"); // Not reference but actual messages
+
+    if (!conversation) {
+      return res.status(200).json({});
+    }
+    const messages = conversation.message;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error retrieving messages:", error.message);
     res.status(500).json({ message: "Server Error" });
   }
 };
