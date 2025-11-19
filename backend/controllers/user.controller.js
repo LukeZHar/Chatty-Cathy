04356bdb6f5e17f153faa89db0df904e37462e83
LukeZHar <luke.z.harris@hotmail.com>
2025-11-19 18:24:51 +1000

    feat: implement conversation management with Zustand; add useGetConversations hook; enhance Conversation and Conversations components with dynamic rendering and emoji support

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,13 +3,14 @@ import User from "../models/user.model.js";
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
+
     const filteredUsers = await User.find({
       _id: { $ne: loggedInUserId },
     }).select("-password");
 
-    res.status(200).json({ users: filteredUsers });
+    res.status(200).json(filteredUsers);
   } catch (error) {
-    console.log("Error retrieving users for sidebar:", error.message);
-    res.status(500).json({ message: "Server Error" });
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
