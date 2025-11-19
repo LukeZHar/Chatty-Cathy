be4877e65d4ca2bbed540a02566dbc958a4504ea
LukeZHar <luke.z.harris@hotmail.com>
2025-11-19 22:22:30 +1000

    feat: implement messaging functionality; add useGetMessages and useSendMessage hooks; enhance MessageContainer and MessageInput components with conversation handling and loading states

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,6 +1,9 @@
+import useGetMessages from "../../hooks/useGetMessages"
 import Message from "./Message"
 
 const Messages = () => {
+  const { loading, messages } = useGetMessages();
+
   return (
     <div className="px-4 flex-1 overflow-auto">
         <Message />
